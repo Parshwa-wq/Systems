3081088cbf68ffce3b2d8919d1ccab9c52b13e9d
DhruvHarani1 <dhruvharani5@gmail.com>
2025-03-02 23:50:57 +0530

    Moved The banking System in Another Folder And Seperated classes in different files and some minnor improvments

---
+++ BankingSystem/BankDetails.java
@@ -0,0 +1,103 @@
+package BankingSystem;
+
+import java.util.Scanner;
+
+public class BankDetails {
+
+    private String accountNumber;       // Account Number of Customer
+    private String name;                // Name of Customer
+    private String accountType;         //Account Type (Savings,Current)
+    private long balance;               // Account Balance
+
+    //Classes And Objects
+    Scanner sc = new Scanner(System.in);
+
+    BankDetails(){
+        //Default Constructor
+    }
+
+    BankDetails(String accountNumber, String name, String accountType, long balance){
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.accountType = accountType;
+        
+        do {
+            this.balance = balance;
+            if (balance<5000) {
+                System.out.println("Minimum deposite Amount is 5000");
+            }
+        } while (balance<5000);
+    }
+
+    // This method is for display the details of the accounts
+    protected void displayAccount() {
+        System.out.println("Name of the account holder is:  " + this.name);
+        System.out.println("Account no is:  " + this.accountNumber);
+        System.out.println("Account type is:  " + this.accountType);
+        System.out.println("Initial balance in the account is:  " + this.balance);
+        sc.nextLine();
+        System.out.println("Press Enter To Continue...");
+        sc.nextLine();
+    }
+    
+    // This method is for deposit money
+    protected void deposit() {
+        long amount;
+        System.out.print("Enter the amount to be deposited ");
+        amount = sc.nextLong();
+        balance = balance + amount;
+    }
+
+    // This method is for withdraw money
+    protected void withdraw() {
+        long amount;
+        System.out.println("Enter the amount to be withdrawn ");
+        amount = sc.nextLong();
+        if (balance >= amount) {
+            balance = balance - amount;
+            System.out.println("Updated balance is " + balance);
+        } else {
+            System.out.println("Your balance is less than the " + amount + "\n" + "Transaction failed");
+        }
+    }
+
+    protected void search(String accountNumber , int count , BankDetails[] users) {
+
+        boolean flag = false;
+        
+        for (int i = 0; i < count; i++) {
+            if (accountNumber.equals(users[i].accountNumber)) {
+                users[i].displayAccount();
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag) {
+            System.out.println("Account Not Found!");
+        }
+    }
+
+    protected void transaction(String accountNumber , int count , BankDetails[] users ,  String transactionType) {
+
+        boolean flag = false;
+        
+        for (int i = 0; i < count; i++) {
+            if (accountNumber.equals(users[i].accountNumber)) {
+               
+                if (transactionType.equals("deposit")) {
+                    users[i].deposit();
+                }
+                if (transactionType.equals("withdraw")) {
+                    users[i].withdraw();
+                }
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag) {
+            System.out.println("Account Not Found!");
+        }
+    }    
+}
---
+++ BankingSystem/BankingApp_BB.java
@@ -0,0 +1,80 @@
+package BankingSystem;
+
+import java.util.*;
+
+class BankingApp {
+    public static void main(String[] args) {
+
+        // Variables
+        int count = 0;
+
+        // Classes and objects
+        Scanner sc = new Scanner(System.in);
+        BankDetails users[] = new BankDetails[100]; // array of users
+        BankDetails tasks = new BankDetails(); // to perform tasks like deposite and withdraw
+
+        int choice;
+        do {
+            System.out.println("\n-------BANKING SYSTEM APPLICATION-------");
+            System.out.println(
+                    "1.Creat A Account\n2.Display all account deatils\n3.Search acc by Account type\n4.Deposit the amount\n5.Withdraw the amount\n6.Exit");
+            System.out.print("Enter (1-6) : ");
+            choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+
+                case 1:
+                    // to create a new account
+                    System.out.print("Enter Account number: ");
+                    String accountNumber = sc.nextLine();
+
+                    System.out.print("Enter Account Type (Saving | Current ):  ");
+                    String accountType = sc.nextLine();
+
+                    System.out.print("Enter Account Holder Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter Initial Balance(MIN 5000): ");
+                    long balance = sc.nextLong();
+                    sc.nextLine();
+
+                    users[count] = new BankDetails(accountNumber , accountType , name , balance);
+                    count++;
+
+                    break;
+                case 2:
+                    for (int i = 0; i < count; i++) {
+                        users[i].displayAccount();
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Enter the account No");
+                    String accountNumberToSearch = sc.next();
+
+                    tasks.search(accountNumberToSearch, count, users);
+                    break;
+
+                case 4:
+                    System.out.println("Enter the Account No ");
+                    String accountNumberToDeposit = sc.next();
+
+                    String transactionType = "deposit";
+                    tasks.transaction(accountNumberToDeposit, count, users, transactionType);
+                    break;
+
+                case 5:
+                    System.out.println("Enter the account no ");
+                    String accountNumberToWithdraw = sc.next();
+
+                    transactionType = "withdraw";
+                    tasks.transaction(accountNumberToWithdraw, count, users, transactionType);
+                    break;
+
+                case 6:
+                    System.out.println("Thank You\n----SEE YOU SOON----");
+                    break;
+            }
+        } while (choice != 6);
+    }
+}
